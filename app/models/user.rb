9656a2fc6f768819bf36ca550911cd9085b01579
achaudhari-vf <achaudhari@verticalfox.com>
2024-07-18 12:00:27 +0530

    Added validation to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
 
+    validates :name , presence: true
+    
+    validates :email, presence: true, uniqueness: true, format: {with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
+
 end
